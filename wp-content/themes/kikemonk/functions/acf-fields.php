5aef2267d93ab9459215035a45e70d7a762da84b
Aencor <kikemura07@gmail.com>
2026-01-26 11:27:36 -0600

    Todo ajustado

--- wp-content/themes/kikemonk/functions/acf-fields.php
+++ wp-content/themes/kikemonk/functions/acf-fields.php
@@ -1,27 +1,7 @@
 <?php
-// Force import from JSON to DB to fix missing fields issue
-if( function_exists('acf_import_field_group') && !get_option('kikemonk_acf_force_import_v3') ):
+// ACF Fields are now managed via JSON files in /acf-json/ directory.
+// This allows them to be synced and edited in the WP Admin.
+// 
+// To sync: Go to Custom Fields > Field Groups > Sync available.
 
-    // Load and import Lugares
-    $json_lugares = file_get_contents(get_stylesheet_directory() . '/acf-json/group_lugares.json');
-    if ($json_lugares) {
-        $group_lugares = json_decode($json_lugares, true);
-        acf_import_field_group($group_lugares);
-    }
-
-    // Load and import Eventos
-    $json_eventos = file_get_contents(get_stylesheet_directory() . '/acf-json/group_eventos.json');
-    if ($json_eventos) {
-        $group_eventos = json_decode($json_eventos, true);
-        acf_import_field_group($group_eventos);
-    }
-
-    // Load and import Theme Settings
-    $json_settings = file_get_contents(get_stylesheet_directory() . '/acf-json/group_theme_settings.json');
-    if ($json_settings) {
-        $group_settings = json_decode($json_settings, true);
-        acf_import_field_group($group_settings);
-    }
-
-    update_option('kikemonk_acf_force_import_v3', true);
-endif;
+// No PHP definitions needed as we have migrated everything to JSON.
